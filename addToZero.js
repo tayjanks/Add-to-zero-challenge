c3c5f832b6861b5e64cbd7e8cbf06a96a9c73df0
tayjanks <taylorjenkins801@gmail.com>
2022-08-31 20:53:01 -0600

    I think I got it but testing

--- addToZero.js
+++ addToZero.js
@@ -2,9 +2,9 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
-
+let equalZero = false
 for (let i=0; i < array.lengh; i++) {
-    for (let j=0;i < array.length; j++)
-    if (array [i] + array [j] === 0){console.log("True")}
-    else [console.log("False")]
-}
+    for (let j=0; j < array.length; j++)
+    {if (array [i] + array [j] === 0){equalZero = true}}}
+
+console.log(equalZero)
